c7dccaa3b2bfca7d719b1fe837d866e143ad7d69
jmansilla <jmansilla@stratechcorp.com>
2025-03-03 16:57:43 -0300

    rm 'uses'

--- app/countries/[country]/page.tsx
+++ app/countries/[country]/page.tsx
@@ -1,7 +1,4 @@
-"use client";
 
-import { useState, useEffect } from "react";
-import { useParams, notFound } from "next/navigation";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Badge } from "@/components/ui/badge";
